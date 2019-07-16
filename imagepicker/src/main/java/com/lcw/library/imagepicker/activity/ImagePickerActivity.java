bd398eb60d435ccf8115bc453cfa702e739e3f19
fzm <fengzhongming@yangufang.com>
2019-07-16 19:43:09 +0800

    调整样式

--- imagepicker/src/main/java/com/lcw/library/imagepicker/activity/ImagePickerActivity.java
+++ imagepicker/src/main/java/com/lcw/library/imagepicker/activity/ImagePickerActivity.java
@@ -2,9 +2,11 @@ package com.lcw.library.imagepicker.activity;
 
 import android.Manifest;
 import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -17,6 +19,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -163,6 +166,8 @@ public class ImagePickerActivity extends BaseActivity implements ImagePickerAdap
         } else {
             mTvTitle.setText(mTitle);
         }
+
+//        ImmersionBar.with(this).init();
         mTvCommit = findViewById(R.id.tv_actionBar_commit);
 
         //滑动悬浮标题相关
@@ -189,12 +194,43 @@ public class ImagePickerActivity extends BaseActivity implements ImagePickerAdap
     }
 
     /**
+     * 通过设置全屏，设置状态栏透明
+     *
+     * @param activity
+     */
+    private void fullScreen(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
+                Window window = activity.getWindow();
+                View decorView = window.getDecorView();
+                //两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                decorView.setSystemUiVisibility(option);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+                //导航栏颜色也可以正常设置
+//                window.setNavigationBarColor(Color.TRANSPARENT);
+            } else {
+                Window window = activity.getWindow();
+                WindowManager.LayoutParams attributes = window.getAttributes();
+                int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+                int flagTranslucentNavigation = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+                attributes.flags |= flagTranslucentStatus;
+//                attributes.flags |= flagTranslucentNavigation;
+                window.setAttributes(attributes);
+            }
+        }
+    }
+
+    /**
      * 初始化控件监听事件
      */
     @Override
     protected void initListener() {
 
-        findViewById(R.id.iv_actionBar_back).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.tv_actionBar_title).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setResult(RESULT_CANCELED);
